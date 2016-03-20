ca5f00f73d99bffe0f6617c516ceeebe700a4848
Joao M <joao.bataglia@gmail.com>
2016-03-20 15:46:54 -0300

    Kine func

--- src/kine.cpp
+++ src/kine.cpp
@@ -2,6 +2,18 @@
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Twist.h>
 
+double w = 0;
+double v = 1;
+
+void callback(const geometry_msgs::TwistConstPtr &msg)
+{
+    
+    w = msg->angular.z;
+    v = msg->linear.x;
+    
+    
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc,argv, "kine");
@@ -9,13 +21,19 @@ int main(int argc, char **argv)
     ros::Publisher pub_left = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed", 1);
     ros::Publisher pub_right = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed", 1);
     ros::Rate loop_rate(60);
+    
+    ros::Subscriber sub = node.subscribe("obstacle/twist", 1, callback);
      
+            
     
      std_msgs::Float64 wr,wl;    
         while(ros::ok())
         {
-            wl.data = 5;
-            wr.data = 5;
+            
+        wr.data  = (v +(0.175*w))/0.1;
+        wl.data  = (v - (0.175*w))/0.1;
+            
+            
             pub_left.publish(wl);
             pub_right.publish(wr);
             
